dd3c6090dfd56492ad85e60a207b58b62db64408
Diego <diego.6.souza@gmail.com>
2013-08-15 17:54:51 -0300

    Layout Email

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,8 @@ class Order < ActiveRecord::Base
   attr_accessible :client_id
   has_many :order_items
   has_many :shoes, :through => :ordem_items
-  belongs_to :clients
-  has_many :enterprise
+  belongs_to :client
+  has_many :enterprises
 
   def grouped_by_enterprise
   	order_items.inject({}) {|memo,item| memo[item.shoe.enterprise.email] ||= []; memo[item.shoe.enterprise.email] << item; memo}
