92637836116d71d883128355fae9c36005174f16
Diego <diego.6.souza@gmail.com>
2013-08-19 08:36:28 -0300

    Layout

--- app/presenters/person_presenter.rb
+++ app/presenters/person_presenter.rb
@@ -1,29 +1,29 @@
 class PersonPresenter
-	delegate :id, :name, to: :"@person"
+	# delegate :id, :name, to: :"@person"
 
-	def initialize(person)
-		@person = person
-	end
+	# def initialize(person)
+	# 	@person = person
+	# end
 
-	def admin
-		@person.admin ? "Sim" : "Não"
-	end
+	# def admin
+	# 	@person.admin ? "Sim" : "Não"
+	# end
 
-	def password
-		"*"*10
-	end
+	# def password
+	# 	"*"*10
+	# end
 
-	def image
-		return "" if !@person.image
-		helpers.image_tag(@person.image.to_s) + "<br>safestyle='clear:both'/>".html_safe
-	end
+	# def image
+	# 	return "" if !@person.image
+	# 	helpers.image_tag(@person.image.to_s) + "<br>safestyle='clear:both'/>".html_safe
+	# end
 
-	def to_s
-		@person.id
-	end
+	# def to_s
+	# 	@person.id
+	# end
 
-	private
-	def helpers
-		ApplicationController.helpers
-	end
+	# private
+	# def helpers
+	# 	ApplicationController.helpers
+	# end
 end
