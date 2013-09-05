f4d73b67cbffa1d6512e1d0fa3d8591109cbedd1
Diego <diego.6.souza@gmail.com>
2013-09-05 16:17:17 -0300

    Finalizando

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Footwear::Application.config.session_store :active_record_store
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
 #Footwear::Application.config.session_store :active_record_store
