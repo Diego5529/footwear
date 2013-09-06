241efd1bc8173ef46c58fa758fad21334122acb1
Eustaquio Rangel <taq@eustaquiorangel.com>
2013-09-06 15:27:29 -0300

    Retornada a autenticação de pessoa por nome.
    Não entendi porque você faz dois por email e um por nome, mas tudo bem. :-)

--- app/models/person.rb
+++ app/models/person.rb
@@ -11,4 +11,8 @@ class Person < ActiveRecord::Base
   scope :by_domain, ->(domain) { where(['name like ?','#{domain}']) }
 
   default_scope -> { order(:name) }
+
+  def self.auth(name,password)
+    where(['name=? and password=?',name,encrypt_password(password)]).first
+  end
 end
