62556d286b21e63267fb37712729009f0017ec5b
Diego <diego.6.souza@gmail.com>
2013-10-01 11:52:41 -0300

    Updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,20 +16,20 @@
 # ]
 
 
-enterprises = [
-  ['Klin', 'klin','diego.6.souza@gmail.com','123456','13.111.111/1311-17','16200-000','Rua','1233','Centro','SP','Birigui','01897748585','../images/enterprises/51.jpg',true],
-  ['Adidas', 'Adidas','diego2@gmail.com','123456','12.111.111/1311-43','16200-000','Rua','1233','Centro','SP','Birigui','01897748585','../images/enterprises/51.jpg',true],
-  ['Puma', 'puma','diego3@gmail.com','123456','12.111.111/1311-54','16200-000','Rua','1233','Centro','SP','Birigui','01897748585','../images/enterprises/51.jpg',true],
-  ['Kidy', 'Kidy','diego4@gmail.com','123456','12.111.111/1311-45','16200-000','Rua','1233','Centro','SP','Birigui','01897748585','../images/enterprises/51.jpg',true]
-]
-
-# shoes = [
-#   ['Nike', 5, 'Mussum ipsum cacilds, vidis litro abertis.', 170.00, DateTime.now, 6,49,13,true]
-#   ['New Shoe', 5, 'Mussum ipsum cacilds, vidis litro abertis.', 170.00, DateTime.now, 6,'http://imagens.passarela.com.br/passarelaEstatico/imagens/produto/8030087467/8030087467_1_Z.JPG',31,true],
-#   ['other', 5, 'Mussum ipsum cacilds, vidis litro abertis.', 170.00, DateTime.now, 6,'http://imagens.passarela.com.br/passarelaEstatico/imagens/produto/8030087467/8030087467_1_Z.JPG',32,true],
-#   ['Metropolitan2', 5, 'Mussum ipsum cacilds, vidis litro abertis.', 170.00, DateTime.now, 6,'http://imagens.passarela.com.br/passarelaEstatico/imagens/produto/8030087467/8030087467_1_Z.JPG',21,true],
-#   ['Metroo', 5, 'Mussum ipsum cacilds, vidis litro abertis.', 170.00, DateTime.now, 6,'http://imagens.passarela.com.br/passarelaEstatico/imagens/produto/8030087467/8030087467_1_Z.JPG',31,true]
+# enterprises = [
+#   ['Klin', 'klin','diego.6.souza@gmail.com','123456','13.111.111/1311-17','16200-000','Rua','1233','Centro','SP','Birigui','01897748585','../images/enterprises/51.jpg',true],
+#   ['Adidas', 'Adidas','diego2@gmail.com','123456','12.111.111/1311-43','16200-000','Rua','1233','Centro','SP','Birigui','01897748585','../images/enterprises/51.jpg',true],
+#   ['Puma', 'puma','diego3@gmail.com','123456','12.111.111/1311-54','16200-000','Rua','1233','Centro','SP','Birigui','01897748585','../images/enterprises/51.jpg',true],
+#   ['Kidy', 'Kidy','diego4@gmail.com','123456','12.111.111/1311-45','16200-000','Rua','1233','Centro','SP','Birigui','01897748585','../images/enterprises/51.jpg',true]
 # ]
+
+shoes = [
+  ['Pé de Klin', 5, 'Este é da Klin.', 170.00, DateTime.now, 6,49,13,true]
+  ['New Shoe', 5, 'Mussum ipsum cacilds, vidis litro abertis.', 170.00, DateTime.now, 6,'http://imagens.passarela.com.br/passarelaEstatico/imagens/produto/8030087467/8030087467_1_Z.JPG',31,true],
+  ['other', 5, 'Mussum ipsum cacilds, vidis litro abertis.', 170.00, DateTime.now, 6,'http://imagens.passarela.com.br/passarelaEstatico/imagens/produto/8030087467/8030087467_1_Z.JPG',32,true],
+  ['Metropolitan2', 5, 'Mussum ipsum cacilds, vidis litro abertis.', 170.00, DateTime.now, 6,'http://imagens.passarela.com.br/passarelaEstatico/imagens/produto/8030087467/8030087467_1_Z.JPG',21,true],
+  ['Metroo', 5, 'Mussum ipsum cacilds, vidis litro abertis.', 170.00, DateTime.now, 6,'http://imagens.passarela.com.br/passarelaEstatico/imagens/produto/8030087467/8030087467_1_Z.JPG',31,true]
+]
 
 
 # client = [
@@ -41,14 +41,14 @@ enterprises = [
 # 	Client.create(name:name,email:email,password:password,cpf:cpf,zip_code:zip_code,address:address,number:number,district:district,city:city,state:state,telephone:telephone)
 # end
 
-enterprises.each do |name,name_social, email, password, cnpj, zip_code, address, number, district, state, city, telephone, image ,permit|
-  Enterprise.create(name:name,name_social:name_social,email:email,password:password,cnpj:cnpj,zip_code:zip_code,address:address,number:number,district:district,state:state,city:city,telephone:telephone,image_title:image,permit:permit)
-end
+# enterprises.each do |name,name_social, email, password, cnpj, zip_code, address, number, district, state, city, telephone, image ,permit|
+#   Enterprise.create(name:name,name_social:name_social,email:email,password:password,cnpj:cnpj,zip_code:zip_code,address:address,number:number,district:district,state:state,city:city,telephone:telephone,image_title:image,permit:permit)
+# end
 
 # category.each do |name|
 #   Category.create(name:name)
 # end
 
-# shoes.each do |name, stock, description, value, date,category,image,enterprise,permit|
-#   Shoe.create(name:name, stock: stock, description: description, value: value, registed: date, category_id: category, image_title:image, enterprise_id: enterprise,permit:permit)  
-# end
+shoes.each do |name, stock, description, value, date,category,image,enterprise,permit|
+  Shoe.create(name:name, stock: stock, description: description, value: value, registed: date, category_id: category, image_title:image, enterprise_id: enterprise,permit:permit)  
+end
