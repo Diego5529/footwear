ac12d0e9cc5e90c40d7312f7ea90cad1a713788d
Diego <diego.6.souza@gmail.com>
2013-11-21 16:31:21 -0200

    Não lembro oq fiz

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ end
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-  gem 'therubyracer', :platforms => :ruby
+  #gem 'therubyracer', :platforms => :ruby
   gem 'uglifier', '>= 1.0.3'
 end
 
--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -9,27 +9,27 @@ class Shoe < ActiveRecord::Base
   has_one :image, dependent: :destroy, as: :imageable
   belongs_to :enterprise
   belongs_to :category
-  
+
   attr_protected :stock
   attr_accessible :category_id, :permit, :description, :enterprise_id, :name, :registed, :value, :stock, :image_title, :data_stream, :lock_version, :order_item_id
-  
+
   validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
   validates :category_id, presence: true
   validates :value, presence: true
   validates :description, presence: true, length: { maximum: 200 }
   validates :stock, presence: true, length:{maximum: 5}
   validate :lock_version
-  
+
   scope :by_enterprise, ->(id) { where(["enterprise_id=?",id]) }
   scope :by_category, ->(id) { where(["category_id=?",id]) }
-  
+
   scope :permited, -> { where(permit: true) }
 
   before_create :check_image
 
   def self.search(search)
     search_condition = "%" + search + "%"
-    find(:all, :conditions => ['name LIKE ? or value LIKE?', search_condition, search_condition])
+    find(:all, conditions: ['name LIKE ? or value LIKE?', search_condition, search_condition])
   end
 
   def sold_out?
